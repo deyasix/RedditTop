1ca5bf193e4c3a2748e481a4b194a99b22de77d8
deyasix <akikidzhan@gmail.com>
2023-02-06 18:59:05 +0200

    adding button for downloading images to gallery + adding functions for downloading images, set access to external storage in android manifest

--- app/src/main/java/com/example/reddittop/PostAdapter.kt
+++ app/src/main/java/com/example/reddittop/PostAdapter.kt
@@ -1,18 +1,29 @@
 package com.example.reddittop
 
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.reddittop.databinding.ListItemBinding
 import com.example.reddittop.model.Post
 import com.squareup.picasso.Picasso
+import java.io.*
+
 
 class PostAdapter(private val context: Context, private val data: MutableList<Post>) :
     RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
-    class PostViewHolder(postLayoutBinding: ListItemBinding) :
+    inner class PostViewHolder(postLayoutBinding: ListItemBinding) :
         RecyclerView.ViewHolder(postLayoutBinding.root) {
         private val binding = postLayoutBinding
 
@@ -28,8 +39,13 @@ class PostAdapter(private val context: Context, private val data: MutableList<Po
                     )
                 )
             }
+            binding.downloadButton.setOnClickListener {
+                val bitmap = getImage(binding.thumbnail)
+                if (bitmap != null) saveToStorage(bitmap)
+            }
         }
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
         return PostViewHolder(
@@ -49,6 +65,48 @@ class PostAdapter(private val context: Context, private val data: MutableList<Po
         data.addAll(listItems)
         val sizeNew = data.size
         notifyItemRangeChanged(size, sizeNew)
+    }
+
+    fun getImage(view: ImageView): Bitmap? {
+        var image: Bitmap? = null
+        try {
+            image = Bitmap.createBitmap(
+                view.measuredWidth, view.measuredHeight, Bitmap.Config.ARGB_8888
+            )
+            val canvas = Canvas(image)
+            view.draw(canvas)
+        } catch (ex: Exception) {
+            ex.localizedMessage?.let { Log.e("Error", it) }
+        }
+        return image
+    }
+
+    fun saveToStorage(bitmap: Bitmap) {
+        val imageName = "${System.currentTimeMillis()}.jpg"
+        var fos: OutputStream? = null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            context.contentResolver?.also { resolver ->
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, imageName)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+                val imageUri: Uri? =
+                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                fos = imageUri?.let {
+                    resolver.openOutputStream(it)
+                }
+            }
+        } else {
+            val imagesDirectory =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDirectory, imageName)
+            fos = FileOutputStream(image)
+        }
+        fos?.use {
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            Toast.makeText(context, "Image was saved to gallery", Toast.LENGTH_LONG).show()
+        }
     }
 
 }
